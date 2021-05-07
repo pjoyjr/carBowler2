c219b7b7e350807b693041597851cf0de0138fd9
Paul Joy <pjoyjr@gmail.com>
2021-05-07 01:35:41 -0400

    refactored

--- js/engine.js
+++ js/engine.js
@@ -14,35 +14,29 @@ var createScene = function() {
                 goToCarSelect = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
 
                 var playBtn = BABYLON.GUI.Button.CreateSimpleButton("playBtn", "Play!");
-                playBtn.width = "40%";
-                playBtn.height = "10%";
-                playBtn.color = "white";
-                playBtn.alpha = .6;
-                playBtn.cornerRadius = 20;
-                playBtn.background = "green";
+                var scoresBtn = BABYLON.GUI.Button.CreateSimpleButton("scoresBtn", "Leaderboard");
+                var quitBtn = BABYLON.GUI.Button.CreateSimpleButton("quitBtn", "Quit");
+                var buttons = [playBtn, scoresBtn, quitBtn];
+
+                for (var i = 0; i < buttons.length; i++) {
+                    buttons[i].width = "35%";
+                    buttons[i].height = "10%";
+                    buttons[i].color = "white";
+                    buttons[i].alpha = .8;
+                    buttons[i].cornerRadius = 20;
+                    buttons[i].background = "blue";
+                    buttons[i].fontSize = "50px";
+                    buttons[i].children[0].color = "white";
+                }
+
                 playBtn.top = "5%";
-                playBtn.fontSizeInPixels = "50px";
+                scoresBtn.top = "20%";
+                quitBtn.top = "35%";
+
                 playBtn.onPointerUpObservable.add(function() {
                     clicks++;
                 });
 
-                var scoresBtn = BABYLON.GUI.Button.CreateSimpleButton("scoresBtn", "Leaderboard");
-                scoresBtn.width = "35%";
-                scoresBtn.height = "10%";
-                scoresBtn.color = "white";
-                scoresBtn.alpha = .6;
-                scoresBtn.cornerRadius = 20;
-                scoresBtn.top = "20%";
-                scoresBtn.background = "blue";
-
-                var quitBtn = BABYLON.GUI.Button.CreateSimpleButton("quitBtn", "Quit");
-                quitBtn.width = "20%";
-                quitBtn.height = "10%";
-                quitBtn.color = "white";
-                quitBtn.alpha = .6;
-                quitBtn.cornerRadius = 20;
-                quitBtn.top = "35%";
-                quitBtn.background = "red";
                 quitBtn.onPointerUpObservable.add(function() {
                     window.close();
                 });
@@ -70,43 +64,30 @@ var createScene = function() {
 
     }
 
-
-    // This creates a basic Babylon Scene object (non-mesh)
+    //Create Scenes
     var scene = new BABYLON.Scene(engine);
+    var scene1 = new BABYLON.Scene(engine);
 
     //Setup Camera and lighting
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, true);
-
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
-
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
-
-    // second scene
-    scene1 = new BABYLON.Scene(engine);
-
-    //Setup Camera and lighting
     var camera1 = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene1);
     camera1.setTarget(BABYLON.Vector3.Zero());
     camera1.attachControl(canvas, true);
 
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
     var light1 = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene1);
     light1.intensity = 0.7;
 
+    // Built in Meshes
+    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+    sphere.position.y = 1; // Move the sphere upward 1/2 its height
     var cube = BABYLON.MeshBuilder.CreateBox('box', { size: 2 }, scene1)
-
 
     createGUI(scene, showScene);
 
-    //runRenderLoop inside a setTimeout is neccesary in the Playground
-    //to stop the PG's runRenderLoop.
     setTimeout(function() {
         engine.stopRenderLoop();
 
@@ -133,10 +114,13 @@ var createScene = function() {
 };
 
 
+/*
+CODE NEEDED TO RUN BABYLON.JS ENGINE AND SCENES
+*/
 var scene = createScene();
 
 engine.runRenderLoop(function() {
-    scene = createScene();
+
     scene.render();
 });
 
