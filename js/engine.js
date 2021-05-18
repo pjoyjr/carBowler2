b2079d2f9d6d337f1c5ab38124075af6fc514b37
Paul Joy <pjoyjr@gmail.com>
2021-05-18 05:47:40 -0400

    rotating meshes

--- js/engine.js
+++ js/engine.js
@@ -1,8 +1,52 @@
+/*
+	for Vector3(x,z,y)
+	x-axis refers to perpendicular to lane
+	z-axis refers to normal to lane
+	y-axis refers to parallel to lane
+*/
+
 const canvas = document.getElementById("renderCanvas");
 
 var currScene = 0;
 var state = 0;
 var mainMenuScene, carSelectScene;
+var bgLayer, playBtn, nextBtn, prevBtn, selectBtn, cam, light;
+var car1Angle = 210,
+    car2Angle = 330,
+    car3Angle = 90,
+    radius = 2.5;
+
+var cube, cone, pill;
+
+var degToRadians = function(degrees) {
+    var radians = degrees * Math.PI / 180;
+    return radians;
+};
+
+var rotateCarSelectionPrev = function() {
+    car1Angle += 120;
+    car2Angle += 120;
+    car3Angle += 120;
+
+    cube.position.x = Math.cos(degToRadians(car1Angle)) * radius;
+    cube.position.z = Math.sin(degToRadians(car1Angle)) * radius - 1;
+    cone.position.x = Math.cos(degToRadians(car2Angle)) * radius;
+    cone.position.z = Math.sin(degToRadians(car2Angle)) * radius - 1;
+    pill.position.x = Math.cos(degToRadians(car3Angle)) * radius;
+    pill.position.z = Math.sin(degToRadians(car3Angle)) * radius - 1;
+}
+var rotateCarSelectionNext = function() {
+    car1Angle -= 120;
+    car2Angle -= 120;
+    car3Angle -= 120;
+
+    cube.position.x = Math.cos(degToRadians(car1Angle)) * radius;
+    cube.position.z = Math.sin(degToRadians(car1Angle)) * radius - 1;
+    cone.position.x = Math.cos(degToRadians(car2Angle)) * radius;
+    cone.position.z = Math.sin(degToRadians(car2Angle)) * radius - 1;
+    pill.position.x = Math.cos(degToRadians(car3Angle)) * radius;
+    pill.position.z = Math.sin(degToRadians(car3Angle)) * radius - 1;
+}
 
 var formatBtn = function(button) {
     button.width = "35%";
@@ -16,14 +60,8 @@ var formatBtn = function(button) {
 };
 
 var createMainMenu = function() {
-    var cam, light, bgLayer, playBtn;
-
     mainMenuScene = new BABYLON.Scene(engine);
-    cam = new BABYLON.FreeCamera("cam", new BABYLON.Vector3(0, 5, -10), mainMenuScene);
-    cam.lockedTarget = new BABYLON.Vector3.Zero();
-    cam.attachControl(canvas, true);
-    light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), mainMenuScene);
-    light.intensity = 0.7;
+    mainMenuScene.createDefaultCameraOrLight(true, true, true);
 
     bgLayer = new BABYLON.Layer('', 'https://raw.githubusercontent.com/pjoyjr/carBowler2/main/img/bg.png', mainMenuScene, true);
 
@@ -40,60 +78,68 @@ var createMainMenu = function() {
 };
 
 var createCarSelect = function() {
-    var nextBtn, prevBtn, selectBtn, backBtn, cam, light;
 
     carSelectScene = new BABYLON.Scene(engine);
     carSelectScene.clearColor = new BABYLON.Color3.Purple();
-    cam = new BABYLON.FreeCamera("cam", new BABYLON.Vector3(0, 5, -10), carSelectScene);
+    cam = new BABYLON.FreeCamera("cam", new BABYLON.Vector3(0, 10, 2), carSelectScene);
     cam.lockedTarget = new BABYLON.Vector3.Zero();
     cam.attachControl(canvas, true);
     light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), carSelectScene);
-    light.intensity = 0.7;
+    // light.intensity = 0.7;
 
-    var cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 2 }, carSelectScene);
-    cube.position.y = 3; // Move the sphere upward 1/2 its width
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2 }, carSelectScene);
-    sphere.position.x = 3; // Move the sphere upward 1/2 its width
+    cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 2 }, carSelectScene);
+    cube.position.x = Math.cos(degToRadians(car1Angle)) * radius;
+    cube.position.z = Math.sin(degToRadians(car1Angle)) * radius - 1;
+    cone = BABYLON.MeshBuilder.CreateCylinder("cone", { diameterTop: .25, height: 1, tessellation: 24 }, carSelectScene);
+    cone.position.x = Math.cos(degToRadians(car2Angle)) * radius;
+    cone.position.z = Math.sin(degToRadians(car2Angle)) * radius - 1;
+    cone.rotation.x = 90;
+    cone.rotation.z = 90;
+    pill = new BABYLON.Mesh.CreateCapsule('pill', { radius: 0.5, height: 2 }, carSelectScene)
+    pill.position.x = Math.cos(degToRadians(car3Angle)) * radius;
+    pill.position.z = Math.sin(degToRadians(car3Angle)) * radius - 1;
+    pill.rotation.x = 90;
+    pill.rotation.z = -90;
 
     carSelectScene.registerBeforeRender(function() {
-
-        cube.rotation.z += .005;
-        cube.rotation.y += 0.01;
+        cube.rotation.y += .005;
+        cone.rotation.y += .005;
+        pill.rotation.y += .005;
     });
 
     carSelectGUI = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI", true, carSelectScene);
     nextBtn = BABYLON.GUI.Button.CreateSimpleButton("nextBtn", ">");
     prevBtn = BABYLON.GUI.Button.CreateSimpleButton("prevBtn", "<");
     selectBtn = BABYLON.GUI.Button.CreateSimpleButton("selectBtn", "Select Car");
-    backBtn = BABYLON.GUI.Button.CreateSimpleButton("backBtn", "Go To Menu");
     formatBtn(nextBtn);
     formatBtn(prevBtn);
     nextBtn.width = "5%";
-    nextBtn.top = "-15%";
-    nextBtn.left = "40%";
+    nextBtn.top = "20%";
+    nextBtn.left = "20%";
     prevBtn.width = "5%";
-    prevBtn.top = "-15%";
-    prevBtn.left = "-40%";
+    prevBtn.top = "20%";
+    prevBtn.left = "-20%";
     formatBtn(selectBtn);
-    formatBtn(backBtn);
-    selectBtn.top = "30%";
-    backBtn.top = "42%";
-
+    selectBtn.top = "42%";
     /*
     selectBtn.onPointerUpObservable.add(function() {
         currScene = 2;
+        
     });
     */
-    backBtn.onPointerUpObservable.add(function() {
-        currScene = 0;
+    prevBtn.onPointerUpObservable.add(function() {
+        rotateCarSelectionPrev();
+    });
+
+    nextBtn.onPointerUpObservable.add(function() {
+        rotateCarSelectionNext();
     });
 
     carSelectGUI.addControl(nextBtn);
     carSelectGUI.addControl(prevBtn);
     carSelectGUI.addControl(selectBtn);
-    carSelectGUI.addControl(backBtn);
 
-    return carSelectScene
+    return carSelectScene;
 }
 
 var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true, disableWebGL2Support: false });
